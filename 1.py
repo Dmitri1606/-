de96b1a182551a45fdd9dffb4d1b1953ba1155e4
Dmitri1606 <dendimvad@gmail.com>
2024-12-05 10:50:00 +0700

    Задание

---
+++ 1.py
@@ -0,0 +1,34 @@
+
+# s = str(input())#1 задача
+# count = 0
+# glasbk = set("аеёиоуыэюяАЕЁИОУЫЭЮЯ")
+# for letter in s:
+#   if letter in glasbk:
+#    count += 1
+# print(count)
+# a=0#втарая задача
+# my_str=str(input())
+# reversed_s = my_str[::-1]
+# print(reversed_s)
+
+# my_str = str(input())#3 задача
+# simv = str(input())
+# index = my_str.find(simv)
+#
+# if index>-1:
+#     print("слово найдено")
+# else:
+#     print("не найдено")
+
+
+# phone=str(input())# 4 задача
+# edited_phone = phone.replace(" ", "_")
+# print(edited_phone)
+
+
+# my_str=str(input())
+# reversed_s = my_str[::-1]
+# if my_str==reversed_s:
+#     print('это палиндром')
+# else:
+#     print('не палиндром')
